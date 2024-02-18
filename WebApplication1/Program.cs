377434721e26b524b6bd7bdb25e92df5232d6fd8
akshayraoch <chakshayrao@gmail.com>
2024-02-18 21:58:36 +0530

    Initial commit and a few endpoints added

---
+++ WebApplication1/Program.cs
@@ -0,0 +1,51 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+
+Note[] notes = new Note[]
+    {
+       new Note( "akshay", "build this app", "nodate"),new Note( "aakash", "learn c#", "today" ),new Note( "charan", "testnote", "today" ) };
+
+
+
+
+
+
+
+
+
+
+app.MapGet("/notes", () =>
+{
+    return notes;
+});
+
+app.MapPost("/createNote", (Note note) =>
+{ 
+    notes = notes.Append(note).ToArray();
+    return notes;
+});
+
+app.MapPatch("/updateNote/{title}", (string title, Note note) =>
+{
+    
+});
+
+app.Run();
+
+record Note(string Title, string note, string dateandtime){}
